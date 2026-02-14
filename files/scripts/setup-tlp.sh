11e3fb611c4aeccb5b4bd50ff101accd67fe85f6
Ivanilton <silvaivanilto@outlook.com>
2026-02-14 18:27:42 +0000

    feat: Update base image to Fedora 43, target KDE Plasma Master/6.5.91+, refine TLP installation, and adjust package lists for a bleeding-edge configuration.

--- files/scripts/setup-tlp.sh
+++ files/scripts/setup-tlp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ouex pipefail
 
-# Install TLP RPM release (Repo)
+# Install Upstream TLP Repo (Linrunner) for latest version (1.9.1+)
 rpm-ostree install https://repo.linrunner.de/fedora/tlp/repos/releases/tlp-release.fc$(rpm -E %fedora).noarch.rpm
--- files/scripts/upgrade-kde-beta.sh
+++ files/scripts/upgrade-kde-beta.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ouex pipefail
 
-# Upgrade KDE packages to beta versions from the @kdesig/kde-beta COPR (only if newer)
+# Upgrade KDE packages to Plasma 6.5.91+ (Master/Beta) from @kdesig/kde-beta COPR
 dnf upgrade -y --enablerepo=copr:copr.fedorainfracloud.org:group_kdesig:kde-beta
